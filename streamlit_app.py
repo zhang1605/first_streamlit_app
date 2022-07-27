5969a2854e6fd0c58095ec120d96c697a51b8816
Michael Zhang <zhang1605@gmail.com>
2022-07-27 09:12:15 -0500

    emoji, dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import pandas as pd
+
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 st.title("My parents New Healthy Dinner")
 
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.header("Breakfast Menu")
+
+st.text("🥣Omega 3 & Blueberry Oatmeal")
+st.text("🥗 Kale, Spinach & Rocket Smoothie")
+st.text("🐔 Hard-Boiled Free-Range Egg")
+st.text("🥑🍞 Avocado Toast")
+
+st.dataframe(my_fruit_list)
