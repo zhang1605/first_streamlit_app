ade68fda8ab01cf48dbdc1f7693270325451c77d
Michael Zhang <zhang1605@gmail.com>
2022-07-27 09:16:08 -0500

    pick

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 st.title("My parents New Healthy Dinner")
 
@@ -14,5 +15,8 @@ st.text("🐔 Hard-Boiled Free-Range Egg")
 st.text("🥑🍞 Avocado Toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
 st.dataframe(my_fruit_list)
