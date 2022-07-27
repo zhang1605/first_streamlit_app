10a57872d0438bc8fccf3e8033d07b40c0cd44eb
Michael Zhang <zhang1605@gmail.com>
2022-07-27 09:12:43 -0500

    add a space

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title("My parents New Healthy Dinner")
 
 st.header("Breakfast Menu")
 
-st.text("🥣Omega 3 & Blueberry Oatmeal")
+st.text("🥣 Omega 3 & Blueberry Oatmeal")
 st.text("🥗 Kale, Spinach & Rocket Smoothie")
 st.text("🐔 Hard-Boiled Free-Range Egg")
 st.text("🥑🍞 Avocado Toast")
