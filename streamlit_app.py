33f28d41675685313b018a4059b8c54ea4e4a3f0
Michael Zhang <zhang1605@gmail.com>
2022-07-27 19:06:24 -0500

    fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import  requests
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
@@ -20,3 +20,6 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/frruit/watermelon").json()
+st.text(fruityvice_response)
